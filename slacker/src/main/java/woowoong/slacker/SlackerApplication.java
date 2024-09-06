623c3180bf2bcf9751132a4ca32255959f431d6d
minyeong <ninja2319@naver.com>
2024-09-06 14:27:49 +0900

    Initial commit

---
+++ slacker/src/main/java/woowoong/slacker/SlackerApplication.java
@@ -0,0 +1,13 @@
+package woowoong.slacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackerApplication.class, args);
+	}
+
+}
---
+++ slacker/src/test/java/woowoong/slacker/SlackerApplicationTests.java
@@ -0,0 +1,13 @@
+package woowoong.slacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

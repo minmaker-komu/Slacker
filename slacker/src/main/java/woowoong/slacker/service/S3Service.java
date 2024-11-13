6a4cf04f3f4844486a18f606ed9e984e299f553b
haeji08 <haejipark88@gmail.com>
2024-11-13 13:54:34 +0900

    LocalTime 수정

--- slacker/src/main/java/woowoong/slacker/service/S3Service.java
+++ slacker/src/main/java/woowoong/slacker/service/S3Service.java
@@ -2,6 +2,7 @@ package woowoong.slacker.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -18,19 +19,41 @@ public class S3Service {
         this.s3Client = s3Client;
     }
 
+//    public String uploadFile(MultipartFile file) throws IOException {
+//        String bucketName = "slacker-image";
+//        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+//
+//        // S3에 파일 업로드
+//        s3Client.putObject(PutObjectRequest.builder()
+//                        .bucket(bucketName)
+//                        .key(fileName)
+//                        .build(),
+//                RequestBody.fromBytes(file.getBytes()));
+//
+//        // S3 URL 생성
+//        return String.format("https://%s.s3.amazonaws.com/%s", bucketName, fileName);
+//    }
+
     public String uploadFile(MultipartFile file) throws IOException {
         String bucketName = "slacker-image";
         String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
 
-        // S3에 파일 업로드
-        s3Client.putObject(PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(fileName)
-                        .build(),
-                RequestBody.fromBytes(file.getBytes()));
+        try {
+            // S3에 파일 업로드
+            s3Client.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(fileName)
+                            .build(),
+                    RequestBody.fromBytes(file.getBytes()));
 
-        // S3 URL 생성
-        return String.format("https://%s.s3.amazonaws.com/%s", bucketName, fileName);
+            // S3 URL 생성
+            return String.format("https://%s.s3.amazonaws.com/%s", bucketName, fileName);
+        } catch (SdkClientException e) {
+            // AWS SDK 관련 예외 처리
+            System.out.println(("S3UploadError S3 업로드 실패: " + e.getMessage()));
+            throw new IOException("S3 업로드 중 오류가 발생했습니다.", e);
+        }
     }
+
 }
 
